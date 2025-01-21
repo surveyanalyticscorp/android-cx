3ed3ee6359abff2a34b8e9fc1db72c862ef879e2
datta-kunde-qp <datta.kunde@questionpro.com>
2025-01-21 12:30:37 +0530

    InteractionActivity refactored - Showing loading inside the activity.

--- CX-Demo_App/cxlib/src/main/java/com/questionpro/cxlib/interaction/InteractionActivity.java
+++ CX-Demo_App/cxlib/src/main/java/com/questionpro/cxlib/interaction/InteractionActivity.java
@@ -1,9 +1,9 @@
 package com.questionpro.cxlib.interaction;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -12,22 +12,27 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import androidx.fragment.app.FragmentActivity;
 
 import com.questionpro.cxlib.R;
-import com.questionpro.cxlib.constants.CXConstants;
+import com.questionpro.cxlib.dataconnect.CXApiHandler;
+import com.questionpro.cxlib.init.CXGlobalInfo;
+import com.questionpro.cxlib.interfaces.QuestionProApiCall;
 import com.questionpro.cxlib.model.CXInteraction;
 import com.questionpro.cxlib.util.CXUtils;
+
+import org.json.JSONObject;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public class InteractionActivity extends FragmentActivity implements MyWebChromeClient.ProgressListener {
+public class InteractionActivity extends FragmentActivity implements MyWebChromeClient.ProgressListener, QuestionProApiCall {
     private final String LOG_TAG="InteractionActivity";
     private ProgressBar progressBar;
+    private ProgressDialog progressDialog;
     private WebView webView;
     private String url = "";
     private CXInteraction cxInteraction;
@@ -36,25 +41,22 @@ public class InteractionActivity extends FragmentActivity implements MyWebChrome
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        cxInteraction =(CXInteraction) getIntent().getSerializableExtra(CXConstants.CX_INTERACTION_CONTENT);
-        url = cxInteraction.url;
-        if(cxInteraction.isDialog) {
+        //cxInteraction =(CXInteraction) getIntent().getSerializableExtra(CXConstants.CX_INTERACTION_CONTENT);
+        //url = cxInteraction.url;
+
+        init();
+
+        getSurveyDetails();
+
+        //launchSurvey();
+    }
+
+    private void init(){
+        if(CXGlobalInfo.isShowDialog(this)) {
             setContentView(R.layout.cx_webview_dialog);
-            RelativeLayout container = (RelativeLayout) findViewById(R.id.topBar);
-            if(!cxInteraction.themeColor.isEmpty()) {
-                container.setBackgroundColor(Color.parseColor(cxInteraction.themeColor));
-            }
+           
         } else {
             setContentView(R.layout.cx_webview_fullscreen);
-            try {
-                RelativeLayout container = (RelativeLayout) findViewById(R.id.topBar);
-                if(!cxInteraction.themeColor.isEmpty()) {
-                    container.setBackgroundColor(Color.parseColor(cxInteraction.themeColor));
-                }
-                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
-                        CXUtils.convertDpToPixel(this, 40));
-                container.setLayoutParams(params);
-            }catch (Exception e){e.printStackTrace();}
         }
         ImageButton closeButton = (ImageButton)findViewById(R.id.closeButton);
         closeButton.setOnClickListener(new View.OnClickListener() {
@@ -67,8 +69,9 @@ public class InteractionActivity extends FragmentActivity implements MyWebChrome
         //CXUtils.lockOrientation(this);
         progressBar =(ProgressBar) findViewById(R.id.progressBar);
         webView = (WebView)findViewById(R.id.surveyWebView);
+
         webView.setWebViewClient(new CXWebViewClient());
-        webView.setWebChromeClient(new MyWebChromeClient(this));
+        webView.setWebChromeClient(new MyWebChromeClient(InteractionActivity.this));
         webView.setVerticalScrollBarEnabled(false);
         webView.setHorizontalScrollBarEnabled(false);
         webView.getSettings().setJavaScriptEnabled(true);
@@ -77,22 +80,77 @@ public class InteractionActivity extends FragmentActivity implements MyWebChrome
         webView.clearCache(true);
         webView.getSettings().setUserAgentString("AndroidWebView");
         webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        webView.setBackgroundColor(Color.WHITE);
 
-        /*int density = (int)getResources().getDisplayMetrics().density;
-        webView.getSettings().setTextZoom(100 * density);*/
+                     /*int density = (int)getResources().getDisplayMetrics().density;
+                    webView.getSettings().setTextZoom(100 * density);*/
         webView.getSettings().setTextZoom(90);
 
-        if(url==null || CXUtils.isEmpty(url)){
+
+    }
+    
+    private void launchSurvey(String url){
+        webView.loadUrl(url);
+    }
+
+    private void getSurveyDetails(){
+        try {
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage("QuestionPro");
+            progressDialog.setMessage("Loading survey...");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+
+            long surveyId = (Long) getIntent().getSerializableExtra("SURVEY_ID");
+
+            CXGlobalInfo.updateCXPayloadWithSurveyId(surveyId);
+            new CXApiHandler(this).execute();
+        }catch (Exception e){
+
+        }
+    }
+
+    @Override
+    public void onSuccess(final String surveyUrl) {
+        Log.d("Datta","Urllll: "+surveyUrl);
+        if(surveyUrl==null || CXUtils.isEmpty(surveyUrl)){
             finish();
         } else{
-            webView.loadUrl(url);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    launchSurvey(surveyUrl);
+                }
+            });
         }
+    }
+
+    @Override
+    public void onError(JSONObject response) {
+        if(null != progressDialog && progressDialog.isShowing()){
+            progressDialog.cancel();
+        }
+        try {
+            if (response.has("error") && response.getJSONObject("error").has("message")) {
+                final String errorMessage = "Error: " + response.getJSONObject("error").getString("message");
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        Toast.makeText(InteractionActivity.this, errorMessage, Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        }catch (Exception e){}
     }
 
     @Override
     public void onUpdateProgress(int progressValue) {
         if(progressBar != null){
             progressBar.setProgress(progressValue);
+            if(progressValue >= 20){
+                if(null != progressDialog && progressDialog.isShowing()){
+                    progressDialog.cancel();
+                }
+            }
             if(progressValue == 100){
                 progressBar.setVisibility(View.GONE);
             }
@@ -106,7 +164,7 @@ public class InteractionActivity extends FragmentActivity implements MyWebChrome
                 finish();
             }
         };
-        worker.schedule(task, 5, TimeUnit.SECONDS);
+        worker.schedule(task, 4, TimeUnit.SECONDS);
     }
     private class CXWebViewClient extends WebViewClient {
         @Override
@@ -133,7 +191,8 @@ public class InteractionActivity extends FragmentActivity implements MyWebChrome
         @Override
         public void onPageFinished(WebView view, String url) {
             progressBar.setVisibility(View.GONE);
-            if(url.contains("#autoClose") || !url.contains("questionpro")){
+            Log.d("Datta","onPageFinished url:"+url);
+            if(url.contains("#autoClose") || !url.contains("questionpro") || url.contains("exitsurvey")){
                 runTimer();
             }
         }
