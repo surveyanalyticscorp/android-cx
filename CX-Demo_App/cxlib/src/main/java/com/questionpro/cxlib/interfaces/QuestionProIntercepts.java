dae7ab83b1ad6a1c7ee69ad2b7536e1e4a97b01c
datta-kunde-qp <datta.kunde@questionpro.com>
2025-04-21 15:42:51 +0530

    Added viewCountRule satisfied callback function in the interface.

--- CX-Demo_App/cxlib/src/main/java/com/questionpro/cxlib/interfaces/QuestionProIntercepts.java
+++ CX-Demo_App/cxlib/src/main/java/com/questionpro/cxlib/interfaces/QuestionProIntercepts.java
@@ -2,4 +2,5 @@ package com.questionpro.cxlib.interfaces;
 
 public interface QuestionProIntercepts {
     void onTimeSpendSatisfied(int  interceptId);
+    void onViewCountRuleSatisfied(int interceptId);
 }
