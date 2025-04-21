b0f340ee5760075935ff99636b77e98a6923c47e
datta-kunde-qp <datta.kunde@questionpro.com>
2025-04-21 15:35:48 +0530

    Added Tag name rule check function in MonitorAppEvents class.

--- CX-Demo_App/cxlib/src/main/java/com/questionpro/cxlib/MonitorAppEvents.java
+++ CX-Demo_App/cxlib/src/main/java/com/questionpro/cxlib/MonitorAppEvents.java
@@ -2,10 +2,15 @@ package com.questionpro.cxlib;
 
 import android.os.Handler;
 import android.os.Looper;
-import android.util.Log;
 
+import com.questionpro.cxlib.enums.InterceptRuleType;
 import com.questionpro.cxlib.interfaces.QuestionProIntercepts;
+import com.questionpro.cxlib.model.Intercept;
 import com.questionpro.cxlib.model.InterceptRule;
+import com.questionpro.cxlib.util.SharedPreferenceManager;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MonitorAppEvents {
 
@@ -41,5 +46,32 @@ public class MonitorAppEvents {
         }
     }
 
+    protected void setTagNameCheckRules(String tagName, SharedPreferenceManager preferenceManager, final QuestionProIntercepts intercepts){
+        if(preferenceManager != null) {
+            int viewCountForTag = preferenceManager.updateViewCountForTag(tagName);
+            //Log.d("Datta", "View count for tag name: "+tagName+" is: "+viewCountForTag);
+            try {
+                JSONObject interceptObj = new JSONObject(preferenceManager.getIntercepts());
+                JSONArray interceptArray = interceptObj.getJSONArray("intercepts");
+                for (int i = 0; i < interceptArray.length(); i++) {
+                    JSONObject jsonObject = interceptArray.getJSONObject(i);
+                    Intercept intercept = Intercept.fromJSON(jsonObject);
+                    //Log.d("Datta", "Intercept Id for tag "+tagName+" : "+intercept.id);
+                    for (InterceptRule rule : intercept.interceptRule) {
+                        if (rule.name.equals(InterceptRuleType.VIEW_COUNT.name()) &&
+                                rule.key.equals(tagName) &&
+                                Integer.parseInt(rule.value) == viewCountForTag) {
+                            //Log.d("Datta", "Key of intercept "+ rule.key+" : "+rule.value);
+                            intercepts.onViewCountRuleSatisfied(intercept.id);
+                            preferenceManager.resetViewCountForTag(tagName);
+                        }
+                    }
+                }
+            } catch (Exception ignored) {
 
+            }
+        }else{
+            // handle the else case
+        }
+    }
 }
