83cf9c7fae0c1b912bbb6d0ee495ef143cf24714
datta-kunde-qp <datta.kunde@questionpro.com>
2025-04-23 13:06:56 +0530

    Update base url to production instead of staging.

--- CX-Demo_App/cxlib/src/main/java/com/questionpro/cxlib/constants/CXConstants.java
+++ CX-Demo_App/cxlib/src/main/java/com/questionpro/cxlib/constants/CXConstants.java
@@ -10,6 +10,9 @@ public class CXConstants {
 
     private static final String CX_TRANSACTION_SURVEY_URL = "/a/api/v2/cx/transactions/survey-url";
     private static final String SURVEYS_URL = "/a/api/v2/surveys/";
+    private static final String GET_MOBILE_INTERCEPTS = "/api/v1/visitor/mobile";
+    private static final String CX_INTERCEPT_SURVEY_URL = "/api/v1/data-mapping/mobile/survey-url";
+    private static final String SUBMIT_SURVEY_FEEDBACK = "/api/v1/visitor/mobile/survey-feedback";
     public static final String PREF_NAME="questionpro_cx";
     //public static final String PREF_KEY_API_KEY="cx_pref_api_key";
     public static final String MANIFEST_KEY_API_KEY="cx_manifest_api_key";
@@ -18,11 +21,10 @@ public class CXConstants {
     public static final String CX_INTERACTION_CONTENT="cx_interaction_content";
 
 
-    public static String getUrl(Context context, String surveyId) {
+    public static String getSurveyUrl(Context context) {
         try {
-            //String type = CXGlobalInfo.getType(context);
-            String dataCenter = CXGlobalInfo.getDataCenter(context);
-            return getBaseUrl(dataCenter) + CX_TRANSACTION_SURVEY_URL;
+            String dataCenter = CXGlobalInfo.getDataCenter();
+            return getBaseUrl(dataCenter) + CX_INTERCEPT_SURVEY_URL;
         }catch (Exception e){
             e.printStackTrace();
         }
@@ -30,26 +32,32 @@ public class CXConstants {
     }
 
     public static String getInterceptsUrl(){
-        return "https://cx-intercept-staging-api.questionpro.com/api/v1/visitor/mobile?";
+        String dataCenter = CXGlobalInfo.getDataCenter();
+        return getBaseUrl(dataCenter) + GET_MOBILE_INTERCEPTS;
+    }
+
+    public static String getFeedbackUrl(){
+        String dataCenter = CXGlobalInfo.getDataCenter();
+        return getBaseUrl(dataCenter) + SUBMIT_SURVEY_FEEDBACK;
     }
 
     private static String getBaseUrl(String dataCenter){
         if(DataCenter.EU.name().equalsIgnoreCase(dataCenter))
-            return "https://api.questionpro.eu";
+            return "https://intercept-api.questionpro.eu";
         if(DataCenter.CA.name().equalsIgnoreCase(dataCenter))
-            return "https://api.questionpro.ca";
+            return "https://intercept-api.questionpro.ca";
         if(DataCenter.SG.name().equalsIgnoreCase(dataCenter))
-            return "https://api.questionpro.sg";
+            return "https://intercept-api.questionpro.sg";
         if(DataCenter.AU.name().equalsIgnoreCase(dataCenter))
-            return "https://api.questionpro.au";
+            return "https://intercept-api.questionpro.au";
         if(DataCenter.AE.name().equalsIgnoreCase(dataCenter))
-            return "https://api.questionpro.ae";
+            return "https://intercept-api.questionpro.ae";
         if(DataCenter.SA.name().equalsIgnoreCase(dataCenter))
             return "https://api.surveyanalytics.com";
         if(DataCenter.KSA.name().equalsIgnoreCase(dataCenter))
-            return "https://api.questionprosa.com";
+            return "https://intercept-api.questionpro.com";
 
-        return "https://api.questionpro.com";
+        return "https://intercept-api.questionpro.com";
     }
 
     public static class JSONUploadFields{
@@ -61,6 +69,7 @@ public class CXConstants {
         public static final String STATUS = "status";
         public static final String RESPONSE = "response";
         public static final String PROJECT = "project";
+        public static final String VISITOR = "visitor";
         public static final String CX_SURVEY_URL = "surveyURL";
         public static final String CORE_SURVEY_URL = "url";
         public static final String IS_DIALOG="isDialog";
