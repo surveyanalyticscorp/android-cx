512230711e36e7fbe6382b3dd4e281e08a4eaefe
datta-kunde-qp <datta.kunde@questionpro.com>
2025-04-24 16:00:18 +0530

    Create a different timer if there are multiple rules are set for spend time monitoring.

--- CX-Demo_App/cxlib/src/main/java/com/questionpro/cxlib/MonitorAppEvents.java
+++ CX-Demo_App/cxlib/src/main/java/com/questionpro/cxlib/MonitorAppEvents.java
@@ -2,6 +2,7 @@ package com.questionpro.cxlib;
 
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 
 import com.questionpro.cxlib.enums.InterceptRuleType;
 import com.questionpro.cxlib.interfaces.IQuestionProRulesCallback;
@@ -12,11 +13,16 @@ import com.questionpro.cxlib.util.SharedPreferenceManager;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MonitorAppEvents {
 
     private Handler handler;
-    private Runnable eventRunnable;
+
     private static MonitorAppEvents mMonitorEvents = null;
+    private final Map<Integer, Runnable> runnableMap = new HashMap<>();
+
 
     private MonitorAppEvents(){}
     public static MonitorAppEvents getInstance(){
@@ -26,24 +32,30 @@ public class MonitorAppEvents {
         return mMonitorEvents;
     }
 
-    protected void appSessionStarted(final int interceptId, InterceptRule rule, final IQuestionProRulesCallback rulesCallback) {
+    protected void appSessionStarted(int interceptId, InterceptRule rule, IQuestionProRulesCallback rulesCallback) {
         handler = new Handler(Looper.getMainLooper());
-        long delay = Long.parseLong(rule.value);
-        eventRunnable = new Runnable() {
+        long delay = Long.parseLong(rule.value) * 1000;
+
+        Runnable runnable = createTimerRunnable(interceptId,  rulesCallback);
+        runnableMap.put(interceptId, runnable);
+        handler.postDelayed(runnable, delay);
+
+    }
+
+    private Runnable createTimerRunnable(final int interceptId, final IQuestionProRulesCallback rulesCallback) {
+        return new Runnable() {
             @Override
             public void run() {
-                // Trigger your event here
                 rulesCallback.onTimeSpendSatisfied(interceptId);
             }
         };
-        handler.postDelayed(eventRunnable, delay * 1000); // 5 minutes in milliseconds
     }
 
-    protected void stopTimer() {
-        if (eventRunnable != null) {
-            handler.removeCallbacks(eventRunnable);
-            eventRunnable = null;
+    protected void stopAllTimers() {
+        for (Runnable runnable : runnableMap.values()) {
+            handler.removeCallbacks(runnable);
         }
+        runnableMap.clear();
     }
 
     protected void setTagNameCheckRules(String tagName, SharedPreferenceManager preferenceManager, final IQuestionProRulesCallback rulesCallback){
