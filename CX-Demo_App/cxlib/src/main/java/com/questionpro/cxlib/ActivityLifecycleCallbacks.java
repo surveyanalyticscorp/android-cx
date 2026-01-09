79ccbd4ede92de9b1ba7c777ae343ec8a70fd650
Datta Kunde <datta.kunde@questionpro.com>
2026-01-09 14:59:41 +0530

    Reduce delay for fetchInterceptSettings call
    Changed the postDelayed timer from 1500ms to 500ms when calling fetchInterceptSettings in ActivityLifecycleCallbacks to improve responsiveness.

--- CX-Demo_App/cxlib/src/main/java/com/questionpro/cxlib/ActivityLifecycleCallbacks.java
+++ CX-Demo_App/cxlib/src/main/java/com/questionpro/cxlib/ActivityLifecycleCallbacks.java
@@ -33,7 +33,7 @@ public class ActivityLifecycleCallbacks implements Application.ActivityLifecycle
                         QuestionProCX.getInstance().fetchInterceptSettings();
                     }catch (Exception e){e.printStackTrace();}
                 }
-            }, 1500);
+            }, 500);
         }
         activityStartCount++;
     }
